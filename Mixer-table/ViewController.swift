310ff3c40f701a8bb46fd163509d8dc155c3ce03
stas-shr <stasshr@gmail.com>
2023-05-12 14:39:33 +0300

    Initial Commit

---
+++ Mixer-table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mixer-table
+//
+//  Created by Стас on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
